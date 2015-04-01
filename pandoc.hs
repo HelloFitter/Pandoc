846c6e620b02152eb4e119710936d3d4a0f4126e
John MacFarlane <jgm@berkeley.edu>
2015-04-01 15:52:32 -0700

    Text.Pandoc.Options:  modifications for image attributes.
    * Added `Ext_common_link_attributes` constructor to `Extension`
      (for link and image attributes).
    * Added this to `pandocExtensions` and `phpMarkdownExtraExtensions`.
    * Added `writerDpi` to `WriterOptions`.
    * pandoc.hs:  Added `--dpi` option.
    * Updated README for `--dpi` and `common_link_attributes` extension.
    
    Patch due to mb21, with some modifications: `writerDpi` is now an
    `Int` rather than a `Double`.

--- pandoc.hs
+++ pandoc.hs
@@ -190,6 +190,7 @@ data Opt = Opt
     , optIgnoreArgs        :: Bool    -- ^ Ignore command-line arguments
     , optVerbose           :: Bool    -- ^ Verbose diagnostic output
     , optReferenceLinks    :: Bool    -- ^ Use reference links in writing markdown, rst
+    , optDpi               :: Int     -- ^ Dpi
     , optWrapText          :: Bool    -- ^ Wrap text
     , optColumns           :: Int     -- ^ Line length in characters
     , optFilters           :: [FilePath] -- ^ Filters to apply
@@ -252,6 +253,7 @@ defaultOpts = Opt
     , optIgnoreArgs            = False
     , optVerbose               = False
     , optReferenceLinks        = False
+    , optDpi                   = 96
     , optWrapText              = True
     , optColumns               = 72
     , optFilters               = []
@@ -447,6 +449,16 @@ options =
                      exitWith ExitSuccess)
                   "FILE")
                   "" -- "Print default data file"
+
+    , Option "" ["dpi"]
+                 (ReqArg
+                  (\arg opt ->
+                    case safeRead arg of
+                         Just t | t > 0 -> return opt { optDpi = t }
+                         _              -> err 31
+                                        "dpi must be a number greater than 0")
+                  "NUMBER")
+                 "" -- "Dpi (default 96)"
 
     , Option "" ["no-wrap"]
                  (NoArg
@@ -1009,8 +1021,8 @@ extractMedia media dir d =
           return $ walk (adjustImagePath dir fps) d
 
 adjustImagePath :: FilePath -> [FilePath] -> Inline -> Inline
-adjustImagePath dir paths (Image lab (src, tit))
-   | src `elem` paths = Image lab (dir ++ "/" ++ src, tit)
+adjustImagePath dir paths (Image attr lab (src, tit))
+   | src `elem` paths = Image attr lab (dir ++ "/" ++ src, tit)
 adjustImagePath _ _ x = x
 
 adjustMetadata :: M.Map String MetaValue -> Pandoc -> IO Pandoc
@@ -1084,6 +1096,7 @@ main = do
               , optIgnoreArgs            = ignoreArgs
               , optVerbose               = verbose
               , optReferenceLinks        = referenceLinks
+              , optDpi                   = dpi
               , optWrapText              = wrap
               , optColumns               = columns
               , optFilters               = filters
@@ -1303,6 +1316,7 @@ main = do
                             writerNumberOffset     = numberFrom,
                             writerSectionDivs      = sectionDivs,
                             writerReferenceLinks   = referenceLinks,
+                            writerDpi              = dpi,
                             writerWrapText         = wrap,
                             writerColumns          = columns,
                             writerEmailObfuscation = obfuscationMethod,
